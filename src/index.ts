f392044e102a772b3d8c8f9dcfbe37c8d0fcd44d
koki-develop <kou.pg.0131@gmail.com>
2025-08-19 23:37:40 +0900

    feat: Implement command

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,43 @@
 import { Command } from "commander";
 import packageJson from "../package.json" with { type: "json" };
+import { type Input, run } from "./main";
 
 const program = new Command();
+
+type Options = {
+  repo: string;
+};
+
+function parseOptions(options: Options): Input {
+  const input: Input = {
+    repository: undefined,
+  };
+
+  // --repo
+  if (options.repo) {
+    const parts = options.repo.split("/");
+    if (parts.length !== 2) {
+      throw new Error(
+        `Invalid repository format: ${options.repo}. Expected format: owner/repo`,
+      );
+    }
+    input.repository = { owner: parts[0]!, name: parts[1]! };
+  }
+
+  return input;
+}
 
 program
   .name("ghatree")
   .version(packageJson.version)
-  .action(() => {
-    console.log("hello world");
+  .option("--repo <repository>", "GitHub repository in owner/repo format")
+  .action(async () => {
+    const options = program.opts<Options>();
+    const input = parseOptions(options);
+    const nodes = await run(input);
+
+    // TODO: tree
+    console.log(JSON.stringify(nodes, null, 2));
   });
 
 program.parse(process.argv);
